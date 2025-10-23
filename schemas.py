137970291610d4e7f7ed8e88db35016b43f9dac1
nikhxxt <nikhxtwork@gmail.com>
2025-10-23 14:42:34 +0530

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,6 +1,20 @@
 from pydantic import BaseModel, Field
 from typing import Optional
 from uuid import UUID
+from datetime import datetime
+
+class PostCreate(BaseModel):
+    text: str = Field(..., min_length=1)
+
+class Post(BaseModel):
+    id: UUID
+    text: str
+    owner_id: int
+    created_at: datetime
+
+class User(BaseModel):
+    id: int
+    username: str
 
 class Like(BaseModel):
     post_id: UUID
