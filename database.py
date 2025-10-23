7f67f095bd364714c7b6d97fd453a34379001a82
nikhxxt <nikhxtwork@gmail.com>
2025-10-23 14:42:52 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,9 @@
-# database.py
-fake_users_db = {
+from typing import Dict, List, Any
+
+fake_users_db: Dict[int, Dict[str, str]] = {
     1: {"username": "john_doe"},
-    2: {"username": "jane_smith"}
+    2: {"username": "jane_doe"}
 }
 
-fake_posts_db = []
+fake_posts_db: List[Dict[str, Any]] = []
+
