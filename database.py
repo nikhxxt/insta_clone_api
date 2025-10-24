8d789c7ba2d158936d8cf9a2f690d00cf62b7e69
nikhxxt <nikhxtwork@gmail.com>
2025-10-24 22:06:01 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,9 +1,15 @@
-from uuid import UUID
+from typing import Dict, List, Any
+from uuid import uuid4
 from datetime import datetime
+
+fake_users_db: Dict[int, Dict[str, str]] = {
+    1: {"username": "john_doe"},
+    2: {"username": "jane_doe"}
+}
 
 fake_posts_db: List[Dict[str, Any]] = [
     {
-        "id": UUID("..."),  # use uuid4() when creating
+        "id": uuid4(),  # generates a valid UUID
         "text": "Hello world!",
         "owner_id": 1,
         "created_at": datetime.utcnow(),
